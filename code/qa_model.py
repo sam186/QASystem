1ea3b80ebabf3cfa625743ccf8ef05e01931f231
Junjie KE <kejunjie1994@gmail.com>
2017-03-10 21:50:00 -0800

    5% data and save

--- code/qa_model.py
+++ code/qa_model.py
@@ -524,7 +524,8 @@ class QASystem(object):
             logging.info("Epoch %d out of %d", epoch + 1, self.config.epochs)
             score = self.run_epoch(session, training_set)
             self.evaluate_answer(session, training_set, vocab, sample=100, log=True)
+            self.evaluate_answer(session, validation_set, vocab, sample=100, log=True)
             # self.validate(session, validation_set)
             # Saving the model
-            # saver = tf.train.Saver()
-            # saver.save(session, train_dir)
+            saver = tf.train.Saver()
+            saver.save(session, train_dir)
