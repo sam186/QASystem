f015a523c709f12062babd6bee32b236a1527cd3
Junjie KE <kejunjie1994@gmail.com>
2017-03-11 11:35:55 -0800

    minibatch in evaluate

--- code/qa_model.py
+++ code/qa_model.py
@@ -525,8 +525,8 @@ class QASystem(object):
         for epoch in range(self.config.epochs):
             logging.info("Epoch %d out of %d", epoch + 1, self.config.epochs)
             score = self.run_epoch(session, training_set)
-            self.evaluate_answer(session, training_set, vocab, sample=20, log=True)
-            self.evaluate_answer(session, validation_set, vocab, sample=20, log=True)
+            self.evaluate_answer(session, training_set, vocab, sample=100, log=True)
+            self.evaluate_answer(session, validation_set, vocab, sample=100, log=True)
             # self.validate(session, validation_set)
             # Saving the model
             saver = tf.train.Saver()
