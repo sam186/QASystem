8cb3c58e5abef4242b8a4825f3f814ac99b797e0
fxia22 <xf1280@gmail.com>
2017-03-18 02:48:01 -0700

    revert to old data

--- code/qa_data.py
+++ code/qa_data.py
@@ -155,7 +155,8 @@ if __name__ == '__main__':
                        pjoin(args.source_dir, "train.question"),
                        pjoin(args.source_dir, "val.context"),
                        pjoin(args.source_dir, "val.question"),
-                       pjoin(args.source_dir, "dev-v1.1.json")])
+                       #pjoin(args.source_dir, "dev-v1.1.json")
+                       ])
     vocab, rev_vocab = initialize_vocabulary(pjoin(args.vocab_dir, "vocab.dat"))
 
     # ======== Trim Distributed Word Representation =======
